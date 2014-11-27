5855f0b36db849a7774032aa58f73d8ae0f2ec4f
sid <koolspy.siddhu@gmail.com>
2014-11-27 23:54:03 +0530

    responsive completed except footer

--- config.rb
+++ config.rb
@@ -5,13 +5,13 @@ require 'compass/import-once/activate'
 http_path = "/"
 
 # You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
+ output_style = :compact 
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+ line_comments = false
 
 
 # If you prefer the indented syntax, you might want to regenerate this
